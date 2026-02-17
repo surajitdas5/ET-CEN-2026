e0fde4b109e2b2308463e32c6c7ee14ff5f45f40
Surajit Das <dassurajit59@yahoo.in>
2026-02-17 09:58:20 +0530

    event project user REST

--- event/utils/db.js
+++ event/utils/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dns from 'dns';
 dns.setServers(['8.8.8.8'])
 
-let DB_URL="mongodb+srv://surajit:surajit@cluster0.pahbl.mongodb.net/cen_event"
+let DB_URL="USE_YOUR_OWN_URL"
 
 async function connectToDB(){
     try {
